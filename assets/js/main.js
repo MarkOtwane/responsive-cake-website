dd94d7fb330a45b495fd15b8f4abf192835a4f07
Josephine Mutiso <jossyjoy2002@gmail.com>
2026-06-15 11:51:58 +0300

    feat: make the home section responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,18 +19,47 @@ if (navClose) {
 
 /*=============== REMOVE MOBILE MENU ===============*/
 
-const navLink = document.querySelector(".nav__link");
+const navLinks = document.querySelectorAll(".nav__link");
 
-const linkAction = () => {
-  const navMenu = document.getElementById("nav--menu");
-
-  navMenu.classList.remove("show-menu");
-};
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+});
 
 /*=============== HOME SWIPER ===============*/
+const swiperHome = new Swiper(".home__swiper", {
+  loop: true,
+  grabCursor: true,
+  speed: 800,
+  effect: "creative",
+
+  creativeEffect: {
+    prev: {
+      translate: ["-120%", 0, -500],
+      rotate: [0, 0, -45],
+      opacity: 0,
+    },
+    next: {
+      translate: ["120%", 0, -500],
+      rotate: [0, 0, 45],
+      opacity: 0,
+    },
+  },
+  autoplay: {
+    delay: 300,
+    disableOnInteraction: false,
+  },
+});
 
 /*=============== CHANGE HEADER STYLES ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== PRODUCTS SWIPER ===============*/
 
